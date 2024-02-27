18cdc938f16881fb406240ef70517ca6fe52894f
Security2431 <artem2431@gmail.com>
2024-02-27 10:05:56 +0200

    fix: update fill day modal UI

--- apps/nextjs/src/app/workspace/(dashboard)/[id]/_components/Projects.tsx
+++ apps/nextjs/src/app/workspace/(dashboard)/[id]/_components/Projects.tsx
@@ -1,13 +1,14 @@
-import { useFormContext } from "react-hook-form";
 import type { UseFieldArrayReturn } from "react-hook-form";
+import clsx from "clsx";
+import { useFormContext } from "react-hook-form";
 
 import type { Prisma } from "@acme/db";
 
+import type { FormData } from "./modal/FilldayModal";
 import Button from "~/app/_components/button";
 import Input from "~/app/_components/form/Input";
 import Heading from "~/app/_components/heading";
 import { initialFormValues } from "./modal/FilldayModal";
-import type { FormData } from "./modal/FilldayModal";
 
 /* Props - <Projects />
 ============================================================================= */
@@ -16,11 +17,16 @@ interface Props {
     reports: UseFieldArrayReturn<FormData>;
   };
   projects: Pick<Prisma.ProjectCreateInput, "id" | "name">[];
+  className?: string;
 }
 
 /* <Projects />
 ============================================================================= */
-export const Projects: React.FC<Props> = ({ fieldArrays, projects }) => {
+export const Projects: React.FC<Props> = ({
+  fieldArrays,
+  projects,
+  className,
+}) => {
   const { register } = useFormContext<FormData>();
   const { prepend, fields } = fieldArrays.reports;
   // Should filter all projects that are contained in the fieldArrays()
@@ -29,7 +35,12 @@ export const Projects: React.FC<Props> = ({ fieldArrays, projects }) => {
   );
 
   return (
-    <aside className="flex w-36 flex-none flex-col gap-4 overflow-y-scroll">
+    <aside
+      className={clsx(
+        "flex w-36 flex-none flex-col gap-4 overflow-y-scroll",
+        className,
+      )}
+    >
       <Heading as="h6">All projects:</Heading>
       <Input
         className="hidden rounded border border-white bg-transparent px-5 py-2"
