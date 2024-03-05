8531110df979660c602528d3e90513f84021e2b9
Security2431 <artem2431@gmail.com>
2024-03-05 11:00:09 +0200

    fix: add Timer autoplay

--- apps/nextjs/src/app/workspace/(dashboard)/[id]/_components/WorkspaceTimer.tsx
+++ apps/nextjs/src/app/workspace/(dashboard)/[id]/_components/WorkspaceTimer.tsx
@@ -1,6 +1,15 @@
 "use client";
 
-import { useEffect, useRef, useState } from "react";
+import {
+  forwardRef,
+  useCallback,
+  useEffect,
+  useImperativeHandle,
+  useRef,
+  useState,
+} from "react";
+import { differenceInMilliseconds, differenceInMinutes } from "date-fns";
+// import Cookies from "js-cookie";
 import { FaPlayCircle, FaStopCircle } from "react-icons/fa";
 import { FaFolderClosed } from "react-icons/fa6";
 
@@ -15,16 +24,57 @@ const parseDescription = (description: string) =>
 ============================================================================= */
 const WorkspaceTimer = () => {
   const [play, setPlay] = useState(false);
+  const [initialTime, setInitialTime] = useState(0);
+  const [startDate, setStartDate] = useState<Date | null>(null);
 
-  const textArr = parseDescription(`- hello
-- world
-- I can do it - together`);
+  //   const textArr = parseDescription(`- hello
+  // - world
+  // - I can do it - together`);
 
-  console.log(textArr);
+  //   console.log(textArr);
+
+  const onStart = () => {
+    setStartDate(new Date());
+    // setStartTime(startTime);
+    // setStartTime(Date.now());
+    // Cookies.set("project-timer", JSON.stringify({ startDate: Date.now() }));
+    // console.log("onStart");
+  };
+
+  const onStop = () => {
+    // setTime(Date.now());
+    if (startDate) {
+      console.log(
+        "onStop",
+        differenceInMinutes(Date.now(), startDate.getTime()),
+      );
+    }
+    // Cookies.remove("project-timer");
+
+    setStartDate(null);
+  };
 
   const togglePlay = () => {
-    setPlay(!play);
+    if (play) {
+      onStop();
+    } else {
+      onStart();
+    }
+
+    setPlay((prevState) => !prevState);
   };
+
+  // Initiate start timer if exists and autoplay
+  useEffect(() => {
+    const timer = undefined;
+    if (timer) {
+      const timeElapsed = Date.now() - timer.startDate.getTime();
+
+      setInitialTime(timeElapsed);
+      setStartDate(timer.startDate);
+      setPlay(true);
+    }
+  }, []);
 
   return (
     <section className="container flex items-stretch gap-4">
@@ -33,7 +83,7 @@ const WorkspaceTimer = () => {
         <FaFolderClosed />
       </Select> */}
 
-      <Stopwatch startTime={Date.now()} />
+      <Stopwatch startTime={initialTime} isRunning={play} />
 
       <Button className="text-4xl" variant="base" onClick={togglePlay}>
         {play ? <FaStopCircle /> : <FaPlayCircle />}
@@ -44,39 +94,74 @@ const WorkspaceTimer = () => {
 
 export default WorkspaceTimer;
 
-const formatTime = (time: number) => {
-  const hours = Math.floor(time / (60 * 60 * 1000));
-  const minutes = Math.floor((time % (60 * 60 * 1000)) / (60 * 1000));
-  const seconds = Math.floor((time % (60 * 1000)) / 1000);
-
-  return { hours, minutes, seconds };
-};
-
-export function Stopwatch({ startTime }: { startTime: number }) {
+const useTimer = () => {
+  const TICK_INTERVAL = 10;
   const intervalId = useRef<ReturnType<typeof setTimeout>>();
   const [time, setTime] = useState(0);
 
-  const padZero = (num: number) => String(num).padStart(2, "0");
+  const formatTime = (time: number) => {
+    const hours = Math.floor(time / (60 * 60 * 1000));
+    const minutes = Math.floor((time % (60 * 60 * 1000)) / (60 * 1000));
+    const seconds = Math.floor((time % (60 * 1000)) / 1000);
 
-  const { hours, minutes, seconds } = formatTime(time);
+    const padZero = (num: number) => String(num).padStart(2, "0");
 
-  useEffect(() => {
+    return {
+      hours: padZero(hours),
+      minutes: padZero(minutes),
+      seconds: padZero(seconds),
+    };
+  };
+
+  const onStart = useCallback((startTime: number) => {
+    if (startTime) {
+      setTime(startTime);
+    }
     const tick = () => {
-      const currentTime = Date.now();
-
-      const timeElapsed = currentTime - startTime;
-
-      setTime(timeElapsed);
+      setTime((time) => time + TICK_INTERVAL);
     };
 
-    intervalId.current = setInterval(tick, 1000);
+    intervalId.current = setInterval(tick, TICK_INTERVAL);
+  }, []);
 
-    return () => clearInterval(intervalId.current);
-  }, [startTime]);
+  const onStop = useCallback(() => {
+    clearInterval(intervalId.current);
+    setTime(0);
+  }, []);
+
+  return {
+    onStart,
+    onStop,
+    formattedTime: formatTime(time),
+  };
+};
+
+export function Stopwatch({
+  startTime,
+  isRunning,
+}: {
+  startTime: number;
+  isRunning: boolean;
+}) {
+  const {
+    formattedTime: { hours, minutes, seconds },
+    onStart,
+    onStop,
+  } = useTimer();
+
+  useEffect(() => {
+    if (isRunning) {
+      onStart(startTime);
+    } else {
+      onStop();
+    }
+
+    return onStop;
+  }, [isRunning, onStart, onStop, startTime]);
 
   return (
     <div className="w-28 text-center text-2xl">
-      {padZero(hours)}:{padZero(minutes)}:{padZero(seconds)}
+      {hours}:{minutes}:{seconds}
     </div>
   );
 }
